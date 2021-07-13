fdcafd7fc732ee29f7d5542f3be398fa9d0e2703
BecaLParker <becaparker@gmail.com>
2021-07-13 10:01:33 +0100

    Adds Header

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const myHeader = screen.getByText(/Beca's Hangman Experiment/i);
+  expect(myHeader).toBeInTheDocument();
 });
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Beca's Hangman Experiment</h1>
+      <p>Could a hint go here?</p>
+    </div>
+  )
+}
+
+export default Header
