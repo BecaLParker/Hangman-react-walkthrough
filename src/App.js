1a4bc5a533b7a5ff0a310d01928760af4896ff88
BecaLParker <becaparker@gmail.com>
2021-07-13 15:41:05 +0100

    Figure component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header'; 
+import Figure from './components/Figure';
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <Figure />
     </>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders Header', () => {
   const myHeader = screen.getByText(/Beca's Hangman Experiment/i);
   expect(myHeader).toBeInTheDocument();
 });
+
+test('renders Figure', () => {
+  render(<App />);
+  const myFigure = screen.getByText(/Death awaits.../i);
+  expect(myFigure).toBeInTheDocument();
+  // TODO: Write a more rigerous test of the figure component being rendered
+});
